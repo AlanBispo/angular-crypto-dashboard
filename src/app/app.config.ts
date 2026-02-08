d825a77bd67442d9fda82f781b5a0f87145382cd
alanbispo <alannmg1@gmail.com>
2026-02-08 18:35:16 -0300

    feat: estilização da tabela e mudança para o padrão ptbr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,20 @@
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
+
+// Importações para o padrão brasileiro
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
 
-    provideHttpClient()
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
