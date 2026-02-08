bb30f8e73c4fa953283dad1ff66b923f0affefb3
alanbispo <alannmg1@gmail.com>
2026-02-08 18:02:44 -0300

    fix: atualiza para provideZonelessChangeDetection estavel no app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
@@ -7,9 +7,9 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
 
-    provideHttpClient() 
+    provideHttpClient()
   ]
 };
